c0645f93ae4ce6cc07601663f1c4e383bbdfbcae
Nguyễn Phúc Vinh <136446610+vinhngph@users.noreply.github.com>
2026-05-17 10:22:33 +0000

    feat: setup explorer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,7 @@ vim.g.mapleader = " "
 
 vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Save file' })
 vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = 'Quit' })
+
+
+vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle NvimTree window" })
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "Focus NvimTree windows" })
